fd7e0000fc078ca242ef3f0c29ff95545add3830
Jordi Prats Verdu <llimonero_4t@hotmail.com>
2023-10-28 16:16:56 +0200

    Add a category select

---
+++ src/components/ui/FilterSelect.jsx
@@ -0,0 +1,20 @@
+import { Select } from "@chakra-ui/react";
+
+
+export const FilterSelect = ({ categories, setSelectedCategory }) => {
+	return (
+		<>
+			<Select
+				variant="flushed"
+				placeholder="Filter by category"
+				onChange={(e) => setSelectedCategory(parseInt(e.target.value))}
+			>
+				{categories.map((category) => (
+					<option key={category.id} value={category.id}>
+						{category.name}
+					</option>
+				))}
+			</Select>
+		</>
+	);
+};
--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -5,6 +5,7 @@ import { EventsCard } from "../components/ui/EventsCard";
 import { AddEventBtn } from "../components/ui/AddEventBtn";
 import { useEventContext } from "../context/EventsDataContext";
 import { SearchInput } from "../components/ui/SearchInput";
+import { FilterSelect } from "../components/ui/FilterSelect";
 
 export const loader = async () => {
 	const events = await fetch("http://localhost:3000/events");
@@ -18,6 +19,7 @@ export const EventsPage = () => {
 	const [categories, setCategories] = useState([]);
 	const { events: initialEvents } = useLoaderData();
 	const [searchText, setSearchText] = useState(""); // Initialize searchText state
+	const [selectedCategory, setSelectedCategory] = useState(null); // Initialize selectedCategory state
 
 	const fetchEvents = async () => {
 		try {
@@ -46,48 +48,64 @@ export const EventsPage = () => {
 		fetchCategories();
 	}, []);
 
-	if (!eventsData || eventsData.length === 0) {
-		return <div>Loading...</div>;
-	} else {
-		// Filter events based on the search text
-		const filteredEvents = eventsData.filter((event) =>
-			event.title.toLowerCase().includes(searchText.toLowerCase())
-		);
+	const filterEvents = (events, category, searchText) => {
+		let filteredEvents = events;
 
-		return (
-			<Container maxW="auto" bg="gray.50" centerContent mt="3em">
-				<Box padding="4" bg="gray.50" color="black" w="80%">
-					<Heading>List of activities</Heading>
-					<AddEventBtn setEvents={fetchEvents} />
-					{/* <div>
-						{eventsData.map((event) => (
-							<Link key={event.id} to={`event/${event.id}`}>
-								<EventsCard
-									event={event}
-									categories={categories}
-									onClick={() => setSelectedActivity(event)}
-								></EventsCard>
-							</Link>
-						))}
-					</div> */}
-					{/* Pass the value and onChange props to the SearchInput component */}
-					<SearchInput
-						value={searchText}
-						onChange={(e) => setSearchText(e.target.value)}
+		if (category) {
+			filteredEvents = filteredEvents.filter((event) =>
+				event.categoryIds.includes(category)
+			);
+		}
+
+		if (searchText) {
+			filteredEvents = filteredEvents.filter((event) =>
+				event.title.toLowerCase().includes(searchText.toLowerCase())
+			);
+		}
+
+		return filteredEvents;
+	};
+
+if (
+	!eventsData ||
+	eventsData.length === 0 ||
+	!categories ||
+	categories.length === 0
+) {
+	return <div>Loading...</div>;
+} else {
+	const filteredEvents = filterEvents(eventsData, selectedCategory, searchText);
+
+	return (
+		<Container maxW="auto" bg="gray.50" centerContent mt="3em">
+			<Box padding="4" bg="gray.50" color="black" w="80%">
+				<Heading>List of activities</Heading>
+				<AddEventBtn setEvents={fetchEvents} />
+				{/* Pass the value and onChange props to the SearchInput component */}
+				<SearchInput
+					value={searchText}
+					onChange={(e) => setSearchText(e.target.value)}
+				/>
+				{/* Add a conditional check to ensure categories is defined before rendering */}
+				{categories && categories.length > 0 ? (
+					<FilterSelect
+						categories={categories}
+						setSelectedCategory={setSelectedCategory}
 					/>
-					<div>
-						{filteredEvents.map((event) => (
-							<Link key={event.id} to={`event/${event.id}`}>
-								<EventsCard
-									event={event}
-									categories={categories}
-									onClick={() => setSelectedActivity(event)}
-								></EventsCard>
-							</Link>
-						))}
-					</div>
-				</Box>
-			</Container>
-		);
-	}
+				) : null}
+				<div>
+					{filteredEvents.map((event) => (
+						<Link key={event.id} to={`event/${event.id}`}>
+							<EventsCard
+								event={event}
+								categories={categories}
+								onClick={() => setSelectedActivity(event)}
+							></EventsCard>
+						</Link>
+					))}
+				</div>
+			</Box>
+		</Container>
+	);
+}
 };
